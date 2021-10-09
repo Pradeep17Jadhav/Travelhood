0daea83573d65e18818d76d987e6451b61b3fba3
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2021-10-09 21:41:14 +0530

    add trek page

---
+++ src/Pages/Trek/Trek.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import './Trek.css';
+
+export default function Trek() {
+    return (
+        <div className="trek">
+
+            <div className="title">
+                <div className="left">
+                    <h1 className="trek-name"></h1>
+                    <div className="location"></div>
+                </div>
+                <div className="right">
+                </div>
+            </div>
+
+            <div className="details">
+                <Grid container spacing={3}>
+                    <Grid key={something} item xs={6} sm={3} md={3}>
+                    </Grid>
+                    <Grid key={something} item xs={6} sm={3} md={3}>
+                    </Grid>
+                    <Grid key={something} item xs={6} sm={3} md={3}>
+                    </Grid>
+                    <Grid key={something} item xs={6} sm={3} md={3}>
+                    </Grid>
+                </Grid>
+            </div>
+
+            <div className="images">
+            </div>
+
+            <div className="highlights">
+            </div>
+
+            <div className="overview">
+            </div>
+            
+            <div className="itinenary">
+            </div>
+
+            <div className="more-details">
+                <div className = "trek-info"></div>
+                <div className = "things-to-carry"></div>
+                <div className = "difficulty"></div>
+            </div>
+
+            <div className="trek-map">
+            </div>
+
+            
+
+        </div>
+    );
+}
